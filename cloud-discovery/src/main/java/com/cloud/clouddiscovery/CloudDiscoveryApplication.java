8ceca4e3cbfdf0220bade4a210a18becbf8066ea
GitHub52Hz <745587722@qq.com>
2020-08-06 10:32:07 +0800

    init

---
+++ cloud-discovery/src/main/java/com/cloud/clouddiscovery/CloudDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.clouddiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDiscoveryApplication.class, args);
+    }
+
+}
